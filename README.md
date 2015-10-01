a2fead3b1fd5fcde3c715de61c58c9b3a8ef23ca
root <root@mysql3.arrahim>
2015-10-01 14:20:11 +0800

    + README.md   Description/Rule to use the script
    * adhoc.sh
      + add automatic create backup folder
      + move script backup into function

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # dolphin_backup
-Common operation script for mysql backup
+================
+
+# Common operation script for mysql backup
+## Requirement
+   * require root access for adhoc.sh eg: sudo adhoc.sh
+   * chmod +x adhoc.sh
+
+## Warning
+   * This script are not for archiving
+   * This will override all file inside backup folder
+   * Do make backup all file inside backup folder before proceed
--- adhoc.sh
+++ adhoc.sh
@@ -19,9 +19,26 @@ trap finish EXIT
 # Temporary dump staging folder
   TMP=$(mktemp -d -t tmp.XXXXXXXXXX)
 
-mysql --defaults-file=conf/my.cnf --batch --skip-column-names -e "show databases" | grep -v -f "conf/ignore.list" > "$TMP/db.list"
+## function to create backup
+function backup() {
+    echo -e "Start doing backup ..."
+    mysql --defaults-file=conf/my.cnf --batch --skip-column-names -e "show databases" | grep -v -f "conf/ignore.list" > "$TMP/db.list"
 
-for db in `cat $TMP/db.list`; do mysqldump --defaults-file=conf/my.cnf --events --dump-date $db  > ./backup/$db.sql;     
-    echo "Dumped $db"; done
-exit
+    for db in `cat $TMP/db.list`; do mysqldump --defaults-file=conf/my.cnf --events --dump-date $db  > ./backup/$db.sql;     
+        echo "Dumped $db"; done
+    exit
+    echo -e "Backup completed"
+}
+
+# check require folder if not exist create new
+if [ ! -d "$PWD/backup/" ]; then
+    mkdir -p "$PWD/backup/"
+    if [ $? -eq 0 ]; then 
+        echo -e "Folder $PWD/backup/ successfully created."
+        backup
+    fi
+else
+    backup
+fi
+
 
