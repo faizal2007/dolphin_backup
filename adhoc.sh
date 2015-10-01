ab9309c213eab1696bdd269aa97e4c55a06f95ac
Mohd Faizal Mohamed Sadri <faizal@sudoers.my>
2015-10-02 05:42:04 +0800

    * adhoc.sh     + ensure script can be run in cronjob

--- adhoc.sh
+++ adhoc.sh
@@ -10,29 +10,33 @@
 
 ## This script require root access
 
+## Initiate Basic variable
+DIR="$(dirname $0)"
+CONF="$DIR/conf"
+BACKUP_FOLD="$DIR/backup"
+BIN="/usr/bin"
+
+TMP=$(mktemp -d -t tmp.XXXXXXXXXX)
+
 ## function to clear tmp file after task completed
 function finish() {
     rm -rf "$TMP"
 }
 trap finish EXIT
 
-# Temporary dump staging folder
-  TMP=$(mktemp -d -t tmp.XXXXXXXXXX)
-
 ## function to create backup
 function backup() {
     echo -e "Start doing backup ..."
-    mysql --defaults-file=conf/my.cnf --batch --skip-column-names -e "show databases" | grep -v -f "conf/ignore.list" > "$TMP/db.list"
+    $BIN/mysql --defaults-file="$CONF/my.cnf" --batch --skip-column-names -e "show databases" | grep -v -f "$CONF/ignore.list" > "$TMP/db.list"
 
-    for db in `cat $TMP/db.list`; do mysqldump --defaults-file=conf/my.cnf --events --dump-date $db  > ./backup/$db.sql;     
-        echo "Dumped $db"; done
+    for db in `cat $TMP/db.list`; do $BIN/mysqldump --defaults-file="$CONF/my.cnf" --events --dump-date $db  > "$BACKUP_FOLD/$db.sql";     
+        echo "Dumped $db to $BACKUP_FOLD/$db.sql"; done
     exit
-    echo -e "Backup completed"
 }
 
 # check require folder if not exist create new
-if [ ! -d "$PWD/backup/" ]; then
-    mkdir -p "$PWD/backup/"
+if [ ! -d "$BACKUP_FOLD/" ]; then
+    mkdir -p "$BACKUP_FOLD/"
     if [ $? -eq 0 ]; then 
         echo -e "Folder $PWD/backup/ successfully created."
         backup
