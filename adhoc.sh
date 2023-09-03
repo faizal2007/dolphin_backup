98bbfdb778928f6112c6acef44a8ad9a19b9cc90
Faizal Sadri <faizal@geekdo.me>
2023-09-03 21:54:09 +0800

    Auto add db ignore list if not exist

--- adhoc.sh
+++ adhoc.sh
@@ -12,6 +12,17 @@
 
 source lib.sh
 
+## create ignore db list if not exist
+if [ ! -e ./conf/ignore.list ]; then
+cat <<EOF> ./conf/ignore.list
+information_schema
+performance_schema
+mysql
+test
+sys
+EOF
+fi
+
 # check require folder if not exist create new
 if [ ! -d "$BACKUP_FOLD/" ]; then
     mkdir -p "$BACKUP_FOLD/"
