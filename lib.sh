320939a75fd33f73ba26f460477aa1e91c7e6a62
Faizal Sadri <faizal@geekdo.me>
2025-04-24 06:21:20 +0800

    change command to mariadb command

--- lib.sh
+++ lib.sh
@@ -7,17 +7,17 @@ BIN="/usr/bin"
 ## function to create backup
 backup() {
     echo -e "Start doing backup ..."
-    $BIN/mysql --defaults-file="$CONF/my.cnf" --batch --skip-column-names -e "show databases" | grep -v -f "$CONF/ignore.list" > "$CONF/db.list"
+    $BIN/mariadb --defaults-file="$CONF/my.cnf" --batch --skip-column-names -e "show databases" | grep -v -f "$CONF/ignore.list" > "$CONF/db.list"
 
     for db in `cat $CONF/db.list`
     do
-        $BIN/mysqldump --defaults-file="$CONF/my.cnf" --events --dump-date $db  > "$BACKUP_FOLD/$db.sql"
+        $BIN/mariadb-dump --defaults-file="$CONF/my.cnf" --events --dump-date $db  > "$BACKUP_FOLD/$db.sql"
         echo "dumped $db to $BACKUP_FOLD/$db.sql"
     done
 }
 
 create_db() {
-    mysql --defaults-file="$CONF/my.cnf" -e "\
+    $BIN/mariadb --defaults-file="$CONF/my.cnf" -e "\
         DROP DATABASE IF EXISTS ${1};
         CREATE DATABASE ${1}
     "
@@ -27,7 +27,7 @@ restore_db() {
     for db in `cat $CONF/db.list`
     do
         create_db $db
-        mysql --defaults-file=$CONF/my.cnf $db < "$BACKUP_FOLD/$db.sql"
+        $BIN/mariadb --defaults-file=$CONF/my.cnf $db < "$BACKUP_FOLD/$db.sql"
         echo "restored $db from $BACKUP_FOLD/$db.sql."
     done
 }
